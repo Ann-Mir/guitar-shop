d42f0ffa47725fc355a7bc459916ecc914767efa
Anna Mironovich <anna.mail787@gmail.com>
2021-12-26 16:42:17 +0300

    renders server data

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,25 @@
+import { useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import { AppRoute } from '../../const';
+import Spinner from '../../spinner/spinner';
+import { getLoadedDataStatus } from '../../store/guitars-data/selectors';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import CataloguePage from '../pages/catalogue-page/catalogue-page';
-import MainPage from '../pages/main/main-page';
+import MainPage from '../pages/main-page/main-page';
 import NotFoundPage from '../pages/not-found-page/not-found-page';
 import UnderConstructionPage from '../pages/under-construction-page/under-construction-page';
 
 
 function App(): JSX.Element {
+
+  const isDataLoaded = useSelector(getLoadedDataStatus);
+
+  if (!isDataLoaded) {
+    return (
+      <Spinner />
+    );
+  }
 
   return (
     <>
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,29 @@
+import { applyMiddleware, createStore } from '@reduxjs/toolkit';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/app/app';
+import { createApi } from './services/api';
+import { fetchGuitarsAction } from './store/api-actions';
+import { rootReducer } from './store/root-reducer';
+import thunk from 'redux-thunk';
+
+
+const api = createApi();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api))));
+
+store.dispatch(fetchGuitarsAction());
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
