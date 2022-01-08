bc8846a4d3dd8386065dfafaeab21264b0d990ea
Anna Mironovich <anna.mail787@gmail.com>
2022-01-08 18:42:06 +0300

    adds errors notifications

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -1,29 +1,44 @@
+import { toast } from 'react-toastify';
 import { APIRoute, OrderOption, QueryParams, SortOption } from '../const';
 import { ThunkActionResult } from '../types/action';
 import { Guitar } from '../types/guitar';
 import { TQueryParams } from '../types/query';
 import { loadGuitars, loadSearchResults, setGuitarsCount, setMaxPrice, setMinPrice } from './actions';
 
+
+const enum ErrorMessage {
+  FetchGuitars = 'Не удалось загрузить данные из каталога',
+  FetchPrice = 'Не удалось загрузить данные о ценах',
+}
+
 const TOTAL_COUNT = 'x-total-count';
 
 export const fetchGuitarsAction =
   (searchParams: string): ThunkActionResult =>
     async (dispatch, _getState, api): Promise<void> => {
-      const response = await api.get<Guitar[]>(`${APIRoute.Guitars}?${searchParams}`);
-      const { data } = response;
-      const totalCount = response.headers[TOTAL_COUNT] as string;
-      const guitarsCount = totalCount ? Number(totalCount) : data.length;
-      dispatch(loadGuitars(data));
-      dispatch(setGuitarsCount(guitarsCount));
+      try {
+        const response = await api.get<Guitar[]>(`${APIRoute.Guitars}?${searchParams}`);
+        const { data } = response;
+        const totalCount = response.headers[TOTAL_COUNT] as string;
+        const guitarsCount = totalCount ? Number(totalCount) : data.length;
+        dispatch(loadGuitars(data));
+        dispatch(setGuitarsCount(guitarsCount));
+      } catch {
+        toast.error(ErrorMessage.FetchGuitars);
+      }
     };
 
 export const searchGuitarsWithParams =
   (queryParams: TQueryParams): ThunkActionResult =>
     async (dispatch, _getState, api): Promise<void> => {
-      const { data } = await api.get<Guitar[]>(APIRoute.Guitars, {
-        params: queryParams,
-      });
-      dispatch(loadSearchResults(data));
+      try {
+        const { data } = await api.get<Guitar[]>(APIRoute.Guitars, {
+          params: queryParams,
+        });
+        dispatch(loadSearchResults(data));
+      } catch {
+        toast.warn(ErrorMessage.FetchGuitars);
+      }
     };
 
 
@@ -38,9 +53,12 @@ export const fetchMinPriceAction =
       queryParams.set(QueryParams.Start, '0');
       queryParams.delete(QueryParams.PriceGte);
       queryParams.delete(QueryParams.PriceLte);
-
-      const { data } = await api.get<Guitar[]>(`${APIRoute.Guitars}?${queryParams.toString()}`);
-      dispatch(setMinPrice(data[0].price));
+      try {
+        const { data } = await api.get<Guitar[]>(`${APIRoute.Guitars}?${queryParams.toString()}`);
+        dispatch(setMinPrice(data[0].price));
+      } catch {
+        toast.warn(ErrorMessage.FetchPrice);
+      }
     };
 
 export const fetchMaxPriceAction =
@@ -53,7 +71,10 @@ export const fetchMaxPriceAction =
       queryParams.set(QueryParams.Limit, '1');
       queryParams.delete(QueryParams.PriceGte);
       queryParams.delete(QueryParams.PriceLte);
-
-      const { data } = await api.get<Guitar[]>(`${APIRoute.Guitars}?${queryParams.toString()}`);
-      dispatch(setMaxPrice(data[0].price));
+      try {
+        const { data } = await api.get<Guitar[]>(`${APIRoute.Guitars}?${queryParams.toString()}`);
+        dispatch(setMaxPrice(data[0].price));
+      } catch {
+        toast.warn(ErrorMessage.FetchPrice);
+      }
     };
